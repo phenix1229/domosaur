8a9e3d48d93c2cf2a63020c644a78a6acb74349b
phenix1229 <paul.garay@codeimmersives.com>
2019-10-31 11:09:46 -0400

    all but strect goal done

--- main.js
+++ main.js
@@ -6,4 +6,8 @@ document.querySelector('#triceratops').addEventListener('click', function(){docu
 document.querySelector('#feathers').addEventListener('click', function(){document.querySelector('#feathers').style.opacity = '50%'})
 document.querySelector('#rattle').addEventListener('click', function(){document.querySelector('#rattle').style.paddingRight = '100px'})
 document.querySelector('#toggle').addEventListener('click', function(){document.querySelector('#row').style.backgroundColor = 'lightblue';})
+document.querySelector('#toggle').addEventListener('dblclick', function(){document.querySelector('#row').style.backgroundColor = 'white';})
+document.querySelector('#t-rex').addEventListener('mouseover', function(){document.querySelector('#t-rex').style.width = '200px';})
+document.querySelector('#t-rex').addEventListener('mouseover', function(){document.querySelector('#t-rex').style.width = '200px';})
+document.querySelector('#t-rex').addEventListener('onmouseout', function(){document.querySelector('#t-rex').style.width = '162px';})
 
